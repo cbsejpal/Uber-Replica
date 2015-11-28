edcfd68a162682d391e738c8e26076313b44bcf9
cbsejpal <cbsejpal@yahoo.in>
2015-11-27 16:31:14 -0800

    added validations for update

--- Uber-Client/public/angular/customerDashboard.js
+++ Uber-Client/public/angular/customerDashboard.js
@@ -41,37 +41,52 @@ app.controller('profile', function($scope, $http) {
 		}
 
 	});
-	$scope.save = function() {
 
-		$http({
-			method : "POST",
-			url : '/updateCustomer',
-			data : {
+	$scope.save = function($event) {
 
-				"email" : $scope.email,
-				"firstName" : $scope.firstName,
-				"lastName" : $scope.lastName,
-				"state" : $scope.state,
-				"city" : $scope.city,
-				"zipCode": $scope.zipCode,
-				"creditCard" : $scope.creditCard,
-				"phoneNumber" : $scope.phoneNumber
-
-			}
-		}).success(function(data) {
-			//checking the response data for statusCode
-			if (data.statusCode == 401) {
-				alert("error");
-			}
-			else{
-
-				//Making a get call to the '/redirectToHomepage' API
-				window.location.assign("/customerDashboard");
-			}
-		}).error(function(error) {
-
-			alert("save error !");
+		angular.forEach($scope.profileUpdate.$error.required, function(field) {
+			field.$setDirty();
 		});
+
+		if($scope.profileUpdate.$error.required){
+			//$event.preventDefault();
+
+			alert("Please fill all the fields before saving");
+		}
+
+		else{
+
+			$http({
+				method : "POST",
+				url : '/updateCustomer',
+				data : {
+
+					"email" : $scope.email,
+					"firstName" : $scope.firstName,
+					"lastName" : $scope.lastName,
+					"state" : $scope.state,
+					"city" : $scope.city,
+					"zipCode": $scope.zipCode,
+					"creditCard" : $scope.creditCard,
+					"phoneNumber" : $scope.phoneNumber
+
+				}
+			}).success(function(data) {
+				//checking the response data for statusCode
+				if (data.statusCode == 401) {
+					alert("error");
+				}
+				else{
+
+					//Making a get call to the '/redirectToHomepage' API
+					window.location.assign("/customerDashboard");
+				}
+			}).error(function(error) {
+
+				alert("save error !");
+			});
+		}
+
 	};
 
 
