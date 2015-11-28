b9a7e06d1eb9bd4e202d2cf408cb73b7b4178b3f
rushilshah <rushil113@gmail.com>
2015-11-28 01:53:49 -0800

    //Filter for drivers implemented

--- Uber-Backend/services/driver.js
+++ Uber-Backend/services/driver.js
@@ -158,14 +158,52 @@ exports.getDriversInRange = function (msg, callback) {
 
     var json_responses;
     Driver.findAll({where: {verifyStatus:true}}).then(function(driver) {
+
+        var filterDriver = [];
+
         if (driver.length > 0) {
-            json_responses = requestGen.responseGenerator(200, {data: driver});
+            var dist = {}, point = {};
+            for (var intPoint = 0; intPoint < driver.length; intPoint = intPoint + 1) {
+                point = driver[intPoint];
+                dist = distance({
+                    lat1: msg.currentLat,
+                    lon1: msg.currentLng,
+                    lat2: point.lat,
+                    lon2: point.lon
+                });
+                if (dist.m < 10) {
+                    point.distance = dist.m;
+                    filterDriver.push(point);
+                }
+                //console.log('Positions:',JSON.stringify(dist))
+            }
+
+
+            json_responses = requestGen.responseGenerator(200, {data: filterDriver});
         } else {
             json_responses = requestGen.responseGenerator(500, {data: "No Driver found"});
         }
         callback(null, json_responses);
     });
 };
+
+function distance(obj) {
+    var R = 6371; // km
+    var dLat = (obj.lat2 - obj.lat1) * Math.PI / 180;
+    var dLon = (obj.lon2 - obj.lon1) * Math.PI / 180;
+    var lat1 = obj.lat1 * Math.PI / 180;
+    var lat2 = obj.lat2 * Math.PI / 180;
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    var m = d * 0.621371;
+    return {
+        km: d,
+        m: m
+    }
+}
 
 
 
