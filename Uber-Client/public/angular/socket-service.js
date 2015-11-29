5040c7573b97a38b4d870b8983e1f64585e9ea00
rushilshah <rushil113@gmail.com>
2015-11-29 03:54:25 -0800

    //Integrated socket io and request ride. Now have to handle it with frontend.

---
+++ Uber-Client/public/angular/socket-service.js
@@ -0,0 +1,26 @@
+app.factory('socket', function ($rootScope) {
+    var socket = io.connect("http://localhost:3000");
+
+    console.log(socket);
+
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
